58fc3001fbab35b77523709498c17c5701443494
rohan_dhumal <rohand2315@gmail.com>
2026-06-18 17:09:07 +0530

    add new model address

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     role = models.CharField(max_length=20,choices=ROLE_CHOICES)
     mobile = models.CharField(max_length=15,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    address = models.TextField(default="")
     
     def __str__(self):
         return self.username
